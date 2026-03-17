65dfaec6d92a5c38d0a9b88b627d3a538df01fac
david970873737 <jesusreyescuellar7@gmail.com>
2026-03-16 20:41:17 -0500

    Delete Mvc-Calculadora directory

--- Mvc-Calculadora/modelo_numero.py
+++
@@ -1,9 +0,0 @@
-class Numero:
-    def __init__(self, valor):
-        self.valor = float(valor)
-
-    def get_valor(self):
-        return self.valor
-    
-    def set_valor(self, valor):
-        self.valor = valor
